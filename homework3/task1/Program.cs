c993d1ae95b1f4c897743285e35b8019a762c73a
Anastasia <yakovleva554682@yandex.ru>
2023-07-14 14:16:49 +0300

    done homework 3

---
+++ homework3/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int number1 = Math.Abs(number);
+string num = number1.ToString();
+char [] array = new char[num.Length]; 
+for (int i = 0; i < num.Length; i++){
+    array[i] = num[i];
+}
+if (array[0] == array[4] && array[1] == array[3]){
+    Console.WriteLine($"Число {number} – это палиндром.");
+}
+else Console.WriteLine($"Число {number} не является палиндромом.");
+
---
+++ homework3/task2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние 
+// между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void Space(int x1, int x2, int y1, int y2, int z1, int z2){
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    Console.Write($"Расстояние между двумя точками в пространстве равно {distance}.");
+}
+
+int GetNumber(string s){
+    Console.Write(s);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = GetNumber("Введите координату X точки А: ");
+int y1 = GetNumber("Введите координату Y точки А: ");
+int z1 = GetNumber("Введите координату Z точки А: ");
+int x2 = GetNumber("Введите координату X точки В: ");
+int y2 = GetNumber("Введите координату Y точки В: ");
+int z2 = GetNumber("Введите координату Z точки В: ");
+Space(x1, x2, y1, y2, z1, z2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void Length(int x){
+//     x = Math.Abs(x);
+//     string num = x.ToString();
+//     Console.Write(num.Length);
+// }
+
+// int GetNumber(string s){
+//     Console.Write(s);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int digit = GetNumber("Введите число: ");
+// Length(digit);
+
+
+ 
